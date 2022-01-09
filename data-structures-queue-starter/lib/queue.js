aeb22ec31a9bb5e97c5e729d8cc1545a56055020
Alpesh Vyas <alpvyas627@gmail.com>
2022-01-09 00:39:53 -0800

    finished queue project

--- data-structures-queue-starter/lib/queue.js
+++ data-structures-queue-starter/lib/queue.js
@@ -22,11 +22,56 @@
 // -----------
 
 class Node {
-
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
 }
 
 class Queue {
+  constructor(front=null, back=null, length=0) {
+    this.front = front;
+    this.back = back;
+    this.length = length;
+  }
 
+  enqueue(item) {
+    let nextItem = new Node();
+    nextItem.value = item;
+    nextItem.next = null;
+
+    if (this.length === 0) {
+      this.front = nextItem;
+      this.back = nextItem;
+    } else {
+      this.back.next = nextItem;
+      this.back = nextItem;
+    }
+    this.length++;
+
+    return this.length;
+  }
+
+  dequeue() {
+    if (this.length === 0) return null;
+    if (this.length === 1) {
+      let nodeToRemove = this.front;
+      this.front = null;
+      this.back = null;
+      this.length--;
+      return nodeToRemove.value;
+    } else if (this.length > 1){
+      let nodeToRemove = this.front;
+      this.front = nodeToRemove.next;
+      this.length--;
+      return nodeToRemove.value;
+    }
+
+  }
+
+  size() {
+    return this.length;
+  }
 }
 
 exports.Node = Node;
